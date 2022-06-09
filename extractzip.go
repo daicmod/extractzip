644642386d487db4bdad94537d360bf3799ca978
大輔 森田 <daicmod@gmail.com>
2022-06-10 03:47:58 +0900

    ✨ 7z対応

--- extractzip.go
+++ extractzip.go
@@ -5,10 +5,45 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+
+	"github.com/bodgit/sevenzip"
 )
 
 func ExtractFromZip(target, src, dest string) error {
 	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	destFile := filepath.Join(dest, target)
+	out, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	for _, f := range r.File {
+		if filepath.Base(f.Name) == target {
+			rc, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer rc.Close()
+
+			if _, err := io.Copy(out, rc); err != nil {
+				return err
+			}
+
+			break
+		}
+	}
+
+	return nil
+}
+
+func ExtractFrom7z(target, src, dest string) error {
+	r, err := sevenzip.OpenReader(src)
 	if err != nil {
 		return err
 	}
--- extractzip_test.go
+++ extractzip_test.go
@@ -15,5 +15,17 @@ func TestExtractFromZip(t *testing.T) {
 	if err != nil {
 		t.Errorf("Not found sample.txt: %v", err)
 	}
+}
+
+func TestExtractFromu7z(t *testing.T) {
+	err := ExtractFrom7z("wkhtmltopdf.exe", "./wkhtmltox-0.12.6-1.mxe-cross-win64.7z", "./")
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+
+	_, err = os.Stat("wkhtmltopdf.exe")
+	if err != nil {
+		t.Errorf("Not found wkhtmltopdf.exe: %v", err)
+	}
 
 }
